000ef1a77ba895b95a632ce48d24d59fd945b42c
Yuen-iMac <rxg9527@sina.cn>
2016-04-02 13:44:59 +0800

    0402 NSURLComponents初始化

---
+++ 03-NSURLComponents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-NSURLComponents
+//
+//  Created by Yuen-iMac on 16/4/2.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03-NSURLComponents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03-NSURLComponents
+//
+//  Created by Yuen-iMac on 16/4/2.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
