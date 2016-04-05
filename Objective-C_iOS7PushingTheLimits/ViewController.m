380d32243f90626078498c2e2e69b3312417cde8
Yuen-iMac <rxg9527@sina.cn>
2016-04-05 16:22:50 +0800

    0405 准备工作

--- Objective-C_iOS7PushingTheLimits/ViewController.m
+++ Objective-C_iOS7PushingTheLimits/ViewController.m
@@ -18,10 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)didTapShowButton:(UIButton *)sender {
+    
 }
 
 @end
