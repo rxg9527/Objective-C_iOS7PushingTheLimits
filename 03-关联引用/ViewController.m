9b30d354333f9e9b6c746d700bfa66217b285c4a
Yuen-iMac <rxg9527@sina.cn>
2016-04-05 17:54:46 +0800

    0405 初始化

---
+++ 03-关联引用/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  03-关联引用
+//
+//  Created by Yuen-iMac on 16/4/5.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
