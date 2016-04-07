b2f92ec91bfaf2e7a7ba156b2fe249030ca2088f
Yuen-rMBP <rxg9527@sina.cn>
2016-04-08 00:15:08 +0800

    0407 0702-TimeConsuming 初始化

---
+++ 0702-TimeConsuming/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  0702-TimeConsuming
+//
+//  Created by Yuen on 16/4/8.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
