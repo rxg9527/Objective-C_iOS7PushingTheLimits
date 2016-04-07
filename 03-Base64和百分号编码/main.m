f43db57bdda3d61c35d6ad166dcfb05611c58719
Yuen-iMac <rxg9527@sina.cn>
2016-04-07 14:59:51 +0800

    0407 Cocoa使用的Base64和百分号编码对Web协议编解码

---
+++ 03-Base64和百分号编码/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  03-Base64和百分号编码
+//
+//  Created by Yuen-iMac on 16/4/7.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        /*
+         Base64编解码
+         [[NSData alloc] initWithBase64EncodedString:<#(nonnull NSString *)#> options:<#(NSDataBase64DecodingOptions)#>];
+         [NSData alloc] base64EncodedStringWithOptions:<#(NSDataBase64EncodingOptions)#>
+         */
+        
+        /*
+         百分号编解码
+         - (NSString *)stringByAddingPercentEncodingWithAllowedCharacters:(NSCharacterSet *)allowedCharacters
+         @property(readonly, copy) NSString *stringByRemovingPercentEncoding
+         */
+    }
+    return 0;
+}
