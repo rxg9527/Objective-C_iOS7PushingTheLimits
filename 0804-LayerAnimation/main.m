8af3cd073e406177cdea6ddd6532cc8d2af57590
Yuen-rMBP <rxg9527@sina.cn>
2016-04-08 22:42:38 +0800

    0408 0804-LayerAnimation初始化

---
+++ 0804-LayerAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0804-LayerAnimation
+//
+//  Created by Yuen on 16/4/8.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
