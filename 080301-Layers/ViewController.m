3c0c5c998bcef6f938cd1fc4358f75e03716762e
Yuen-rMBP <rxg9527@sina.cn>
2016-04-08 21:55:53 +0800

    0408 直接设置图层CALayer的内容

--- 080301-Layers/ViewController.m
+++ 080301-Layers/ViewController.m
@@ -17,6 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *image = [UIImage imageNamed:@"pushing"];
+    self.view.layer.contentsScale = [UIScreen mainScreen].scale;
+    self.view.layer.contentsGravity = kCAGravityCenter; // 类似于contentMode
+    self.view.layer.contents = (id)image.CGImage;
 }
 
 - (void)didReceiveMemoryWarning {
