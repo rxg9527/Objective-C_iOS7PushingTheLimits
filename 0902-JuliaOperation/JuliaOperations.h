c8aab8224be2146d43b34b24ba81f6db636d275f
Yuen-rMBP <rxg9527@sina.cn>
2016-04-09 16:29:25 +0800

    0409 所有必需的数据都会在开始之前传递给操作。操作在运行过程中不需要和别的对象交互，所以就不需要加锁 操作完成后结果保存在自身 ivar中，让使用者在 completionBlock中根据 operation 的对象取出结果，这样也是避免加锁。 main 方法中定期检查 isCancelled 属性，以便在接收到取消事件的时候能够退出。这点很重要。可以在循环中检查，在任何相对容易停止 operation 的地方都可以检查。

--- 0902-JuliaOperation/JuliaOperations.h
+++ 0902-JuliaOperation/JuliaOperations.h
@@ -12,6 +12,11 @@
 
 @interface JuliaOperations : NSOperation
 
+/**
+ *  所有必需的数据都会在开始之前传递给操作。操作在运行过程中不需要和别的对象交互，所以就不需要加锁
+    操作完成后结果保存在自身 ivar中，让使用者在 completionBlock中根据 operation 的对象取出结果，这样也是避免加锁。
+    main 方法中定期检查 isCancelled 属性，以便在接收到取消事件的时候能够退出。这点很重要。可以在循环中检查，在任何相对容易停止 operation 的地方都可以检查。
+ */
 @property (nonatomic, readwrite, assign) NSUInteger width;
 @property (nonatomic, readwrite, assign) NSUInteger height;
 @property (nonatomic, readwrite, assign) complex long double c;
