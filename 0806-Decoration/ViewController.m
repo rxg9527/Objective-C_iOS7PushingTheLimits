59a68d88c2f84724327d13eb2132551d510f4546
Yuen-rMBP <rxg9527@sina.cn>
2016-04-09 14:36:16 +0800

    0409 CALayer圆角、彩色边线和阴影

--- 0806-Decoration/ViewController.m
+++ 0806-Decoration/ViewController.m
@@ -17,11 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(100, 100, 100, 100);
+    layer.cornerRadius = 10;
+    layer.backgroundColor = [UIColor redColor].CGColor;
+    layer.borderColor = [UIColor blueColor].CGColor;
+    layer.borderWidth = 2;
+    layer.shadowOpacity = 0.5;
+    layer.shadowOffset = CGSizeMake(3.0, 3.0);
+    [self.view.layer addSublayer:layer];
+    
+    layer = [CALayer layer];
+    layer.frame = CGRectMake(150, 150, 100, 100);
+    layer.cornerRadius = 10;
+    layer.backgroundColor = [[UIColor greenColor] CGColor];
+    layer.borderWidth = 2;
+    layer.shadowOpacity = 0.5;
+    layer.shadowOffset = CGSizeMake(3.0, 3.0);
+    [self.view.layer addSublayer:layer];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
