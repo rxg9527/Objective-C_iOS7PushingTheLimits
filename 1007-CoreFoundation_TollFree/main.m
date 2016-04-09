38f167f16d098ceb16992fec789a9f766f1397ff
Yuen-rMBP <rxg9527@sina.cn>
2016-04-09 20:59:41 +0800

    0409      这里需要用__bridge 转换。现在不是把 cfArray 传给一个检查过的函数，而是将其转换成一个 id 对象并向它发送一个消息

--- 1007-CoreFoundation_TollFree/main.m
+++ 1007-CoreFoundation_TollFree/main.m
@@ -22,9 +22,23 @@ static void testTollFree() {
     printf("count=%lu %ld\n", count, CFArrayGetCount((__bridge CFArrayRef)nsArray));
 }
 
+static void testTollFreeReverse() {
+    PrintFunction();
+    CFMutableArrayRef cfArray = CFArrayCreateMutable(NULL, 0, &kCFTypeArrayCallBacks);
+    CFArrayAppendValue(cfArray, CFSTR("FOO"));
+    /**
+     *  output
+     1
+     这里需要用__bridge 转换。现在不是把 cfArray 传给一个检查过的函数，而是将其转换成一个 id 对象并向它发送一个消息
+     */
+    NSLog(@"%ld", [(__bridge id)cfArray count]);
+    CFRelease(cfArray);
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         testTollFree();
+        testTollFreeReverse();
     }
     return 0;
 }
