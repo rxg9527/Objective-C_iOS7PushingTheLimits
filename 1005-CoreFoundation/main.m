0400be24b7b59531de791672f37acd4164eb2011
Yuen-rMBP <rxg9527@sina.cn>
2016-04-09 18:56:36 +0800

    0409 CoreFoundation初始化

---
+++ 1005-CoreFoundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  1005-CoreFoundation
+//
+//  Created by Yuen on 16/4/9.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
