da29c515f67e3bf9705bf5ef1f47de8b6273bb5c
Yuen-rMBP <rxg9527@sina.cn>
2016-04-10 15:38:43 +0800

    0410 初始化

---
+++ 2102-AttributedString/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  2102-AttributedString
+//
+//  Created by Yuen on 16/4/10.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
