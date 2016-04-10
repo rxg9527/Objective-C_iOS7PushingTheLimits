da91603fd8ef8e5cc4b083d1c1b1af553e07385b
Yuen-rMBP <rxg9527@sina.cn>
2016-04-10 15:41:54 +0800

    0410 AttributedString

--- 2102-AttributedString/ViewController.m
+++ 2102-AttributedString/ViewController.m
@@ -10,18 +10,39 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSString *string = @"Be Bold! And a little color wouldn’t hurt either.";
+    
+    NSDictionary *attrs = @{NSFontAttributeName: [UIFont systemFontOfSize:14]};
+    
+    NSMutableAttributedString * as = [[NSMutableAttributedString alloc] initWithString:string
+                                                attributes:attrs];
+    
+    [as addAttribute:NSFontAttributeName
+               value:[UIFont boldSystemFontOfSize:14]
+               range:[string rangeOfString:@"Bold!"]];
+    
+    [as addAttribute:NSForegroundColorAttributeName
+               value:[UIColor blueColor]
+               range:[string rangeOfString:@"little color"]];
+    
+    [as addAttribute:NSFontAttributeName
+               value:[UIFont systemFontOfSize:18]
+               range:[string rangeOfString:@"little"]];
+    
+    [as addAttribute:NSFontAttributeName
+               value:[UIFont fontWithName:@"Papyrus" size:20]
+               range:[string rangeOfString:@"color"]];
+    
+    self.label.attributedText = as;
 }
 
 @end
