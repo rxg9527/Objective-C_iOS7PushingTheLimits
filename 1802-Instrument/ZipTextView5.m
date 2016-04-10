a7831a31436169664758bb02ee9838a452601863
Yuen-rMBP <rxg9527@sina.cn>
2016-04-10 15:19:44 +0800

    0410 TODO CoreAnimation

---
+++ 1802-Instrument/ZipTextView5.m
@@ -0,0 +1,21 @@
+//
+//  ZipTextView5.m
+//  Objective-C_iOS7PushingTheLimits
+//
+//  Created by Yuen on 16/4/10.
+//  Copyright © 2016年 Yuen-iMac. All rights reserved.
+//
+
+#import "ZipTextView.h"
+
+@implementation ZipTextView
+
+//TODO: 使用 Core Animation工具
+- (id)initWithFrame:(CGRect)frame text:(NSString *)text {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+@end
